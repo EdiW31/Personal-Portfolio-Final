fe900c6cde16914a7be46589596a4764cec9311e
EdiW31 <129823940+EdiW31@users.noreply.github.com>
2024-08-13 14:01:00 +0300

    added new project

--- src/Components/Portfolio Cards/portfolioc.jsx
+++ src/Components/Portfolio Cards/portfolioc.jsx
@@ -2,10 +2,50 @@ import "./portfolio.scss";
 import extensionImage from "./../../assets/extension.png";
 import tetris from "./../../assets/tetris.png";
 import rolodex from "./../../assets/rolodex.png";
+import bikePro from "./../../assets/Screenshot 2024-08-13 at 11.02.08.png";
 const portfolioc = () => {
   return (
     <div className="container-lg portfoliocard px-5 mt-5" id="portfolio">
       <h1 className="color_and_pos m-3">Portfolio.</h1>
+      <article className="postcard light blue">
+        <a
+          className="postcard__img_link"
+          href="https://github.com/EdiW31/online-shop-fe-bike-pro"
+        >
+          <img className="postcard__img" src={bikePro} alt="Image Title" />
+        </a>
+        <div className="postcard__text t-dark">
+          <h1 className="postcard__title blue">
+            <a href="https://github.com/EdiW31/Chrome-Tab-Saver-Extension">
+              BikePro.
+            </a>
+          </h1>
+          <div className="postcard__subtitle small"></div>
+          <div className="postcard__bar"></div>
+          <div className="postcard__preview-txt">
+            Bike Pro is an online Shop I developed using technologies like
+            NestJs and React, with TypeScript. This has been one of the biggest
+            projects of mine because it has been a challenge for me, I had no
+            idea what NestJs was or anything but in the end I finished the
+            project and now I know A LOT more then before, I know new
+            technologies and I developed a new way of thinking and I also
+            learned how to debug more eficiently then before. I got the help I
+            needed from the firm where I went as an intern, they helped me a
+            lot. I created an online shop abt bikes where you can add to
+            favorite and do many things like checkout with the Stripe
+            techonolgy, I created an Admin Panel using the Guards from NestJs
+            and I can say that the Frontend Design turned out fine, done with
+            Tailwind Css.
+          </div>
+          <ul className="postcard__tagbox">
+            <li className="tag__item play blue">
+              <a href="https://github.com/EdiW31/Chrome-Tab-Saver-Extension">
+                GitHub Code
+              </a>
+            </li>
+          </ul>
+        </div>
+      </article>
       <article className="postcard light blue">
         <a
           className="postcard__img_link"
